608c267ce8c8fea972518ecdf4b7a640140007d6
HectorUlisesStamatio <ulisesstamatio@gmail.com>
2023-03-10 09:22:07 -0600

    [UPDATE] Más detalles en messages

--- SeguridadProyecto/src/main/java/mx/edu/utez/controller/admin/AdminPersonasController.java
+++ SeguridadProyecto/src/main/java/mx/edu/utez/controller/admin/AdminPersonasController.java
@@ -1,7 +1,7 @@
 package mx.edu.utez.controller.admin;
 
+import javax.persistence.EntityNotFoundException;
 
-import lombok.extern.slf4j.Slf4j;
 import mx.edu.utez.model.Personas;
 import mx.edu.utez.repo.PersonasRepository;
 import mx.edu.utez.util.Message;
@@ -13,82 +13,95 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import javax.persistence.EntityNotFoundException;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
-@RequestMapping("/api/admin/personas")
+@RequestMapping({"/api/admin/personas"})
 public class AdminPersonasController {
+    private static final Logger log = LoggerFactory.getLogger(AdminPersonasController.class);
     private final PersonasRepository personasRepository;
-
-    private final static Logger logger = LoggerFactory.getLogger(AdminPersonasController.class);
+    private static final Logger logger = LoggerFactory.getLogger(AdminPersonasController.class);
 
     @Autowired
     public AdminPersonasController(PersonasRepository personasRepository) {
         this.personasRepository = personasRepository;
     }
 
-    @GetMapping("/listar")
+    @GetMapping({"/listar"})
     ResponseEntity<Object> listar() {
         try {
-            logger.warn("Algo ocurr");
-            return new ResponseEntity(new Message("Personas registradas", "success", personasRepository.findAll()), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity(new Message("Ocurrió un error al listar", "failed"), HttpStatus.BAD_REQUEST);
+            logger.info("Se mostró exitosamente a las personas registradas");
+            return new ResponseEntity(new Message("Personas Mostradas Correctamente", "Se mostró exitosamente a las personas mostradas", "success", this.personasRepository.findAll(), 200), HttpStatus.OK);
+        } catch (Exception var2) {
+            logger.warn("No se pudo mostrar el listado de las personas");
+            logger.warn(var2.toString());
+            return new ResponseEntity(new Message("Ocurrió un error al listar", "No se pudieron listar a las personas", "failed", (Object) null, 400), HttpStatus.BAD_REQUEST);
         }
-
     }
 
-    @GetMapping("/{id}")
+    @GetMapping({"/{id}"})
     ResponseEntity<Object> get(@PathVariable Integer id) {
         try {
-            //optional
-            Personas persona = personasRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-            return new ResponseEntity(new Message("Persona consultada", "success", persona), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity(new Message("Ocurrió un error al consultar", "failed"), HttpStatus.BAD_REQUEST);
+            Personas persona = (Personas) this.personasRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+            log.info("Se consultó correctamente a la persona");
+            return new ResponseEntity(new Message("Persona consultada", "La persona se consultó exitosamente", "success", persona, 200), HttpStatus.OK);
+        } catch (Exception var3) {
+            logger.warn("No se pudo consultar a la persona que se buscaba");
+            logger.warn(var3.toString());
+            return new ResponseEntity(new Message("Ocurrió un error al consultar", "La persona no se pudo consultar debido a un error inesperado.", "failed", (Object) null, 400), HttpStatus.BAD_REQUEST);
         }
-
     }
 
-    @PostMapping("/")
+    @PostMapping({"/"})
     ResponseEntity<Object> crear(@RequestBody @Validated PersonasDTO personasDTO) {
         try {
-            Personas personas = new ModelMapper().map(personasDTO, Personas.class);
-            Personas per = personasRepository.save(personas);
-            return new ResponseEntity(new Message("Persona Insertada", "success", per), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity(new Message("Ocurrió un error al insertar", "failed"), HttpStatus.BAD_REQUEST);
+            Personas personas = (Personas) (new ModelMapper()).map(personasDTO, Personas.class);
+            personas = (Personas) this.personasRepository.saveAndFlush(personas);
+            log.info("Se insertó correctamente a la persona");
+            return new ResponseEntity(new Message("Persona Creada", "Se creó exitosamente a la persona", "success", personas, 200), HttpStatus.OK);
+        } catch (Exception var4) {
+            log.warn("No se pudo insertar a la persona");
+            log.warn(var4.toString());
+            return new ResponseEntity(new Message("Ocurrió un error al crear a la persona", "La persona no se pudo crear debido a un error inesperado.", "failed", (Object) null, 400), HttpStatus.BAD_REQUEST);
         }
     }
 
-    @PutMapping("/{id}")
+    @PutMapping({"/{id}"})
     ResponseEntity<Object> actualizar(@PathVariable Integer id, @RequestBody @Validated PersonasDTO personasDTO) {
         try {
-            Personas personasFromDb = personasRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+            Personas personasFromDb = (Personas) this.personasRepository.findById(id).orElseThrow(EntityNotFoundException::new);
             ModelMapper mapper = new ModelMapper();
             mapper.map(personasDTO, personasFromDb);
-            personasRepository.save(personasFromDb);
-            return new ResponseEntity(new Message("Persona Actualizada Correctamente", "success", personasFromDb), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity(new Message("Ocurrió un error al Actualizar", "failed"), HttpStatus.BAD_REQUEST);
+            personasFromDb = this.personasRepository.saveAndFlush(personasFromDb);
+            log.info("Se actualizó correctamente a la persona");
+            return new ResponseEntity(new Message("Persona Actualizada Correctamente", "La persona se actualizó satisfactoriamente", "success", personasFromDb, 200), HttpStatus.OK);
+        } catch (Exception var5) {
+            log.warn("No se pudo actualizar a la persona");
+            log.warn(var5.toString());
+            return new ResponseEntity(new Message("Ocurrió un error al actualizar a la persona", "La persona no se pudo actualizar debido a un error inesperado.", "failed", (Object) null, 400), HttpStatus.BAD_REQUEST);
         }
-
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/{id}")
+    @DeleteMapping({"/{id}"})
     ResponseEntity<Object> eliminar(@PathVariable Integer id) {
         try {
-            Personas personas = personasRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-            personasRepository.delete(personas);
-            return new ResponseEntity(new Message("Persona Eliminada Correctamente", "success", personas), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity(new Message("Ocurrió un error al Eliminar", "failed"), HttpStatus.BAD_REQUEST);
+            Personas personas = (Personas) this.personasRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+            this.personasRepository.delete(personas);
+            log.info("Se eliminó correctamente a la persona");
+            return new ResponseEntity(new Message("Persona Eliminada Correctamente", "La persona se eliminó satisfactoriamente", "success", (Object) null, 200), HttpStatus.OK);
+        } catch (Exception var3) {
+            log.warn("No se pudo eliminar a la persona");
+            log.warn(var3.toString());
+            return new ResponseEntity(new Message("Ocurrió un error al eliminar a la persona", "La persona no se pudo eliminar debido a un error inesperado.", "failed", (Object) null, 400), HttpStatus.BAD_REQUEST);
         }
     }
 }
--- SeguridadProyecto/src/main/java/mx/edu/utez/util/Message.java
+++ SeguridadProyecto/src/main/java/mx/edu/utez/util/Message.java
@@ -1,15 +1,185 @@
 package mx.edu.utez.util;
 
-import lombok.*;
+import lombok.NonNull;
 
-@Data
-@RequiredArgsConstructor
-@AllArgsConstructor
-@NoArgsConstructor
 public class Message {
-    @NonNull
-    private String text;
-    @NonNull
-    private String type;
+    private @NonNull String title;
+    private @NonNull String message;
+    private @NonNull String type;
     private Object result;
+    private int code;
+
+    public @NonNull String getTitle() {
+        return this.title;
+    }
+
+    public @NonNull String getMessage() {
+        return this.message;
+    }
+
+    public @NonNull String getType() {
+        return this.type;
+    }
+
+    public Object getResult() {
+        return this.result;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
+    public void setTitle(final @NonNull String title) {
+        if (title == null) {
+            throw new NullPointerException("title is marked non-null but is null");
+        } else {
+            this.title = title;
+        }
+    }
+
+    public void setMessage(final @NonNull String message) {
+        if (message == null) {
+            throw new NullPointerException("message is marked non-null but is null");
+        } else {
+            this.message = message;
+        }
+    }
+
+    public void setType(final @NonNull String type) {
+        if (type == null) {
+            throw new NullPointerException("type is marked non-null but is null");
+        } else {
+            this.type = type;
+        }
+    }
+
+    public void setResult(final Object result) {
+        this.result = result;
+    }
+
+    public void setCode(final int code) {
+        this.code = code;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) {
+            return true;
+        } else if (!(o instanceof Message)) {
+            return false;
+        } else {
+            Message other = (Message) o;
+            if (!other.canEqual(this)) {
+                return false;
+            } else if (this.getCode() != other.getCode()) {
+                return false;
+            } else {
+                label61:
+                {
+                    Object this$title = this.getTitle();
+                    Object other$title = other.getTitle();
+                    if (this$title == null) {
+                        if (other$title == null) {
+                            break label61;
+                        }
+                    } else if (this$title.equals(other$title)) {
+                        break label61;
+                    }
+
+                    return false;
+                }
+
+                label54:
+                {
+                    Object this$message = this.getMessage();
+                    Object other$message = other.getMessage();
+                    if (this$message == null) {
+                        if (other$message == null) {
+                            break label54;
+                        }
+                    } else if (this$message.equals(other$message)) {
+                        break label54;
+                    }
+
+                    return false;
+                }
+
+                Object this$type = this.getType();
+                Object other$type = other.getType();
+                if (this$type == null) {
+                    if (other$type != null) {
+                        return false;
+                    }
+                } else if (!this$type.equals(other$type)) {
+                    return false;
+                }
+
+                Object this$result = this.getResult();
+                Object other$result = other.getResult();
+                if (this$result == null) {
+                    if (other$result != null) {
+                        return false;
+                    }
+                } else if (!this$result.equals(other$result)) {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Message;
+    }
+
+    public int hashCode() {
+        int result = 1;
+        result = result * 59 + this.getCode();
+        Object $title = this.getTitle();
+        result = result * 59 + ($title == null ? 43 : $title.hashCode());
+        Object $message = this.getMessage();
+        result = result * 59 + ($message == null ? 43 : $message.hashCode());
+        Object $type = this.getType();
+        result = result * 59 + ($type == null ? 43 : $type.hashCode());
+        Object $result = this.getResult();
+        result = result * 59 + ($result == null ? 43 : $result.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Message(title=" + this.getTitle() + ", message=" + this.getMessage() + ", type=" + this.getType() + ", result=" + this.getResult() + ", code=" + this.getCode() + ")";
+    }
+
+    public Message(final @NonNull String title, final @NonNull String message, final @NonNull String type) {
+        if (title == null) {
+            throw new NullPointerException("title is marked non-null but is null");
+        } else if (message == null) {
+            throw new NullPointerException("message is marked non-null but is null");
+        } else if (type == null) {
+            throw new NullPointerException("type is marked non-null but is null");
+        } else {
+            this.title = title;
+            this.message = message;
+            this.type = type;
+        }
+    }
+
+    public Message(final @NonNull String title, final @NonNull String message, final @NonNull String type, final Object result, final int code) {
+        if (title == null) {
+            throw new NullPointerException("title is marked non-null but is null");
+        } else if (message == null) {
+            throw new NullPointerException("message is marked non-null but is null");
+        } else if (type == null) {
+            throw new NullPointerException("type is marked non-null but is null");
+        } else {
+            this.title = title;
+            this.message = message;
+            this.type = type;
+            this.result = result;
+            this.code = code;
+        }
+    }
+
+    public Message() {
+    }
 }
